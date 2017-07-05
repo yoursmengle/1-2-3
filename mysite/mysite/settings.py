b34b58fa72eb98e6df6a7e73cd41d7802faeb399
root <root@iZwz9exxqofq68ds39xgsmZ.(none)>
2017-07-05 15:57:20 +0800

    add the app123

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -105,7 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUGAGE_CODE = 'zh-cn'
 
 TIME_ZONE = 'UTC'
 
