9635f79e977cb205cc7e3d81452afd435d5fe3ae
jhzhou <yoursmengle@126.com>
2017-07-06 23:38:22 +0800

    add the classes User and Question

--- mysite/app123/models.py
+++ mysite/app123/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=64)
+    mobile = models.CharField(max_length=16)
+    email = models.EmailField()
+
+class Question(models.Model):
+    owner = models.ForeignKey(User)
+    content = models.CharField(max_length = 4096)
+    create_date = models.DateField()
+
+
